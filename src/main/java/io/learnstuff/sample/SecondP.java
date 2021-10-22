3e2193a97c65a811fcea5b146c8e6ff738ca33a8
Chifor Gabriela-Mihaela <gabriela.chifor@yahoo.com>
2021-10-22 21:47:57 +0300

    my first class

---
+++ src/main/java/io/learnstuff/sample/SecondP.java
@@ -0,0 +1,25 @@
+package io.learnstuff.sample;
+
+
+
+public class SecondP {
+
+	public static void main(String[] args) {
+					//1.array number & string
+					
+				//int [] numbers = {15,16,21,33};
+				//for (int i=0; i<numbers.length;i++) {
+					//System.out.println(numbers[i]+" se afla pe pozitia " +(i+1));
+				//}
+				//System.out.println();
+				
+				
+				//String[] names = {"Davin","Roger","Antoinette","Paul"};
+				//for (int i=0; i<numbers.length;i++) {
+					//System.out.println(names[i]+" se afla pe pozitia " +(i+1));
+				//}
+		
+		
+	}
+
+}
